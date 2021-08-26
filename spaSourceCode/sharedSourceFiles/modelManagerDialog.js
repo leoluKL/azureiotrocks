d2768a33feca634975d3fb36256a48731d618fd4
Leo Lu <leolu@microsoft.com>
2021-08-26 18:48:28 +0800

    test devop

--- spaSourceCode/sharedSourceFiles/modelManagerDialog.js
+++ spaSourceCode/sharedSourceFiles/modelManagerDialog.js
@@ -714,9 +714,9 @@ modelManagerDialog.prototype.listModels=async function(shouldBroadcast){
         this.modelList.append(zeroModelItem)
         zeroModelItem.css("cursor","default")
 
-        var createSampleModelsButton = $('<button class="w3-button w3-amber w3-hover-pink w3-border" style="margin:10%;font-size:1em">Create Sample Models</button>')
-        this.modelList.append(createSampleModelsButton) 
-        createSampleModelsButton.on("click", () => { }) 
+        //var createSampleModelsButton = $('<button class="w3-button w3-amber w3-hover-pink w3-border" style="margin:10%;font-size:1em">Create Sample Models</button>')
+        //this.modelList.append(createSampleModelsButton) 
+        //createSampleModelsButton.on("click", () => { }) 
     
     }else{
         this.tree = new simpleTree(this.modelList, {
